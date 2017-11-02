3b374b52495e87a6c2c369a5bac7069d6a322ac8
ab053658 <AB053658wangyanwu@bjbangye.com>
2017-11-02 19:19:45 +0800

    XpathiImage

---
+++ XpathiImage/XpathiImage/pipelines.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import scrapy
+from scrapy.exceptions import DropItem
+from scrapy.pipelines.images import ImagesPipeline
+import re 
+
+
+class XpathiimagePipeline(object):
+    def process_item(self, item, spider):
+        return item
+class MyImagesPipeline(ImagesPipeline):
+    def file_path(self, request, response=None, info=None):
+        """
+        :param request: 每一个图片下载管道请求
+        :param response:
+        :param info:
+        :param strip :清洗Windows系统的文件夹非法字符，避免无法创建目录
+        :return: 每套图的分类目录
+        """
+        item = request.meta['item']
+        folder = item['title']
+        folder_strip = strip(folder)
+        image_guid = request.url.split('?')[-2].split('/')[-1]
+        filename = u'full/{0}/{1}'.format(folder_strip, image_guid)
+        return filename
+    
+    def get_media_requests(self, item,info):
+        yield scrapy.Request(item['detailURL'],meta={'item': item})
+
+
+    def item_completed(self,results,item,info):
+        path=[x['path'] for ok,x in results if ok]
+        if not path:
+            raise DropItem('Item contains no images')
+        print("function item_completed")
+        print(u'正在保存图片：', item['detailURL'])
+        print (u'主题', item['title'])
+        yield item
+
+        
+def strip(path):
+    """
+    :param path: 需要清洗的文件夹名字
+    :return: 清洗掉Windows系统非法文件夹名字的字符串
+    """
+    path = re.sub(r'[？\\*|“<>:/]', '', str(path))
+    return path
---
+++ XpathiImage/XpathiImage/spiders/xpathimage.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import scrapy
+
+#import sys
+from XpathiImage.items import XpathiimageItem
+from scrapy.http import Request
+import re
+from scrapy import Selector
+
+#sys.setdefaultencoding('utf-8')
+
+class MyImageSpider(scrapy.Spider):
+    name = "xpathimage"
+    allowed_domains = ["www.moko.cc"]
+    
+    
+    def start_requests(self):
+        '''
+        输入：
+        输出：启动爬虫
+        用途：用于多页爬取。
+        '''
+        start_urls = ['http://www.moko.cc/channels/post/23/1.html']
+#        print(start_urls)
+        for url in start_urls:
+            
+            yield Request(url,callback=self.parseOne)
+#        start_urls = ['http://www.moko.cc/channels/post/23/1.html']
+#        print(start_urls)
+#        Request(start_urls,callback=self.parseOne)
+    
+    def parseOne(self, response):
+        """
+        输入：response
+        输出:启动下一级爬虫。
+        用途：将目录页的相册标题和src+url放到一个大的list里，启动下一级爬虫。
+        """
+        src='http://www.moko.cc'
+        items=[]
+        item=XpathiimageItem()
+#        pattern=re.compile('<div class="cover" cover-text="(.*?)">.*?href="(.*?)".*?</div>',re.S)
+##text='<div class="cover" cover-text="来一波小清新"><a href="/post/1270140.html" hidefocus="true" target="_blank"><img src2="http://img.mb.moko.cc/2017-10-20/cbf84483-0eb6-45e0-b1c1-80f18a210e5a.jpg" alt="迷醉儿作品《来一波小清新》"/></a></div>'
+#        mains=re.findall(pattern,response.text)
+#        for main in mains:
+#            item={}
+#            print(main)
+#            item['title']=main[0]
+#            item['siteURL']=src+main[1]
+#            items.append(item)
+        mylinks=response.xpath('//div[contains(@class,"cover")]')
+        
+        for index,link  in enumerate(mylinks):
+            args=(index, link.xpath('@cover-text').extract(), link.xpath('a/@href').extract())
+            title=args[1]
+            print(type(title))
+            url=args[2]
+            if title!=[]:
+                item['title']=title
+                item['siteURL']=src+url[0]
+                print(item['title'],item['siteURL'])
+                items.append(item)
+            
+        for item in items:
+            #用meta传入下一层
+            yield Request(url=item['siteURL'],meta={'item1':item},callback=self.parseTwo)        
+#        items['image_urls']=response.xpath('//div[@class="swipeboxEx"]/div[@class="list"]/a/img/@data-original').extract()
+    
+    def parseTwo(self,response):
+        print("intoparseTwo")
+        
+        item=XpathiimageItem()
+        item2=response.meta['item1']
+        picpattern=re.compile('<p class="picBox"><img src2="(http.*?)".*?</p>')
+        URLs=re.findall(picpattern,response.text)
+        for URL in URLs:
+            item['detailURL']=URL
+            item['title']=item2['title']
+            yield item
